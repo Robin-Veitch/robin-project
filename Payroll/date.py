6a79fe52aad73d09b8a0ee583ce4d45ad19354ab
Robin Veitch <robin.john.veitch@gmail.com>
2020-04-01 17:06:16 +0100

    added year function

--- Payroll/date.py
+++ Payroll/date.py
@@ -1,1 +1,13 @@
-import datetime
+from datetime import *import 
+
+def age(date1,date2):
+    """
+    Returns the number of whole years between 
+    the given dates
+    """
+    delta=date1-date2
+    years=delta.days/365.24
+    years=int(years)
+    return years
+
+
